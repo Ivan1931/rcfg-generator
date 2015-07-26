d69764a6cb04f9c0099ec6774fb3e3e9f90b6ca9
Jeroen-Schmidt <jeroen_schmidt@msn.com>
2015-07-26 22:41:14 +0200

    Added 3 functions
    - No testing done as of yet
    - Comments added for clarity
    1 function in production.py along with the 2 other function outlines
    - 2 functions in grammar.py

--- rcfg/grammar.py
+++ rcfg/grammar.py
@@ -12,3 +12,37 @@ class Grammar(object):
         self.terminals = terminals
         self.start_symbol = start_symbol
         self.productions = productions
+
+    def possible_Production(self, str):
+        """Create list of possible productions
+
+        Arguments:
+        str -- list of symbols representing the string
+        """
+
+        self.str = str
+        self.possibleProductionList = []
+
+        for x in productions:
+            if x.can_transform(str):
+                possibleProductionList.append(x)
+
+        return possibleProductionList
+
+    def is_valid(self, str):
+        """Create list of possible future strings that have been produced using
+        the productions obtained in possible_Production
+
+        Arguments:
+        str -- list of symbols representing the string
+        """
+
+        self.str = str
+        self.possibleProductionList = possible_Production(str)
+        self.futureString = []
+
+        for x in possibleProductionList:
+            newProduction = x.transform_symbols(str)
+            futureString.append(newProduction)
+
+        return futureString
--- rcfg/production.py
+++ rcfg/production.py
@@ -1,6 +1,7 @@
 class Production:
     """Deals with a production"""
     def __init__(self, permitting, forbidding, trigger_variable, transform_to):
+
         """Creates a new production
         Arguments:
         permitting -- List of permitting variables. All of these must be present
@@ -11,10 +12,45 @@ class Production:
                             function. This variable gets substituted in all
                             in transitions
         transform_to -- Right hand side of the transition function. In a string
-                        all instances of a trigger_variable will be replaced by
+                        (represented as a list of symboles) all instances of a
+                        trigger_variable will be replaced by
                         instances of transform_to.
         """
+
         self.permitting = permitting
         self.forbidding = forbidding
         self.trigger_variable = trigger_variable
         self.transform_to = transform_to
+
+
+    def can_transform(self, str):
+        """ Checks if production permitting and forbidding conditions are meet
+
+        Arguments:
+        str -- List of symboles that production is to be applied to
+        """
+
+        return true
+
+    def trigger_variable_locations(self, str):
+        """ Determines the possitions of all trigger_variables present in string
+
+        Arguments:
+        str -- List of symboles that production is to be applied to
+        """
+
+        return []
+
+    def transform_symbols(self, str):
+        """Performs a production on string
+
+        Arguments:
+        str -- List of symboles that production is to be applied to
+        """
+
+        self.str = str
+        self.possition = trigger_variable_locations(str).[0]
+
+        str = s[:possition-1] + transform_to + s[possition:]
+
+        return str
